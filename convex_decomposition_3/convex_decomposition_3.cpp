6fe8d3f4d1f65e05730090bf8658e5af9ba1838e
Iuro Nascimento <iurobpn@proton.me>
2022-11-20 16:45:57 -0300

    Update CMakeLists file for CGAl 5.x

--- convex_decomposition_3/convex_decomposition_3.cpp
+++ convex_decomposition_3/convex_decomposition_3.cpp
@@ -1,3 +1,9 @@
+/** MyMEXFunction
+ * c = MyMEXFunction(a,b);
+ * Adds offset argument a to each element of double array b and
+ * returns the modified array c.
+ */
+
 #include <iostream>
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 #include <CGAL/Polyhedron_3.h>
@@ -9,25 +15,9 @@
 #include <list>
 #include <vector>
 #include <thread>
-#include "mex.h"
-#include "mex_main.h"
-#include "matrix.h"
+#include "mex.hpp"
+#include "mexAdapter.hpp"
 
-class mystream : public std::streambuf {
-protected:
-	virtual std::streamsize xsputn(const char *s, std::streamsize n) { mexPrintf("%.*s", n, s); return n; }
-	virtual int overflow(int c=EOF) { if (c != EOF) { mexPrintf("%.1s", &c); } return 1; }
-};
-
-class scoped_redirect_cout {
-public:
-	scoped_redirect_cout() { old_buf = std::cout.rdbuf(); std::cout.rdbuf(&mout); }
-	~scoped_redirect_cout() { std::cout.rdbuf(old_buf); }
-private:
-	mystream mout;
-	std::streambuf *old_buf;
-};
-scoped_redirect_cout mycout_redirect;
 
 typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
 typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
@@ -48,10 +38,6 @@ struct MeshPolyhedron {
 	FacetList facets;
 
 	MeshPolyhedron(){}
-	/* MeshPolyhedron(VertexList vs, FacetList fs) { */
-	/* 	this->vertices = vs; */
-	/* 	this->facets = fs; */
-	/* } */
 };
 
 template <class HDS>
@@ -105,206 +91,231 @@ public:
     }
 };
 
-Polyhedron createPolyhedron(double *verticesRaw, int num_vertices, const mxArray *prhs);
-std::list<Polyhedron> decompose(Nef_polyhedron N);
-std::list<Polyhedron> decompose(Polyhedron P);
-void poly2mesh(Polyhedron P, MeshPolyhedron &MP);
-int find(VertexList vs, Point p);
+using namespace matlab::data;
+using matlab::mex::ArgumentList;
 
+class MexFunction : public matlab::mex::Function {
+public:
+	void operator()(ArgumentList outputs, ArgumentList inputs) {
+		checkArguments(outputs, inputs);
+		// TypedArray<double> inx = std::move(inputs[0]);
+		// const size_t numRows = inx.getDimensions()[0];
+		// double* x = new double[inx.getNumberOfElements()];
+		// memcpy(x,&*inx.begin(),sizeof(float)*inx.getNumberOfElements());
 
-void __mexFunction__( int nlhs, mxArray *plhs[],
-                  int nrhs, const mxArray *prhs[] )
-{
-	double *verticesRaw, *y;
-	size_t mrowsV, ncolsV, mrowsi, ncolsi;
+		// Polygon_List partition_polys = 	optimal_convex_partition_2(numRows,x);
+		// int N = 2;partition_polys.size();
 
-	/* Check for proper number of arguments. */
-	mwSize num_poly_in = nrhs/2; //number of input polygons
-	// std::cout << "num poly in: " << num_poly_in << std::endl;
-	if(nrhs<2) {
-		mexErrMsgIdAndTxt( "MATLAB:convex_decomposition_3:invalidNumInputs",
-	    				"At least two inputs required.");
-	} else if(nlhs > 1) {
-		mexErrMsgIdAndTxt( "MATLAB:convex_decomposition_3:maxlhs",
-	    				"Too many output arguments.");
-	}
-	Nef_polyhedron Nf(Nef_polyhedron::EMPTY); 
-	for (int i = 0; i < nrhs; i=i+2) {
-		/* The input must be a noncomplex 4x3 double matrix.*/
-		mrowsV = mxGetM(prhs[i]);
-		ncolsV = mxGetN(prhs[i]);
+		double *verticesRaw;
+		int Nin = inputs.size();
 
-		// mexPrintf("rows:%d, cols:%d\n",mrows,ncols);
-		if( !mxIsDouble(prhs[i]) || mxIsComplex(prhs[i]) || !(mrowsV>4 && ncolsV==3) ) {
-			mexErrMsgIdAndTxt( "MATLAB:convex_decomposition_3:inputNotBigEnough",
-					    "Input must be at least 4x3 double matrix.");
+		std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
+		ArrayFactory f;
+		/* Check for proper number of arguments. */
+		size_t num_poly_in = Nin/2.0; //number of input polygons
+
+		Nef_polyhedron Nf(Nef_polyhedron::EMPTY); 
+		for (int i = 0; i < Nin; i = i + 2) {
+			size_t numRows = inputs[i].getDimensions()[0];
+			size_t numCols = inputs[i].getDimensions()[1];
+			if (inputs[i].getType() != ArrayType::DOUBLE ||
+					inputs[i].getType() == ArrayType::COMPLEX_DOUBLE ||
+					!(numRows > 4 && numCols == 3)) {
+				matlabPtr->feval(u"error",
+						0,
+						std::vector<Array>({ f.createScalar("It must be at least a 4x3 double matrix") }));
+			}
+			TypedArray<double> Vi = std::move(inputs[i]);
+			verticesRaw = new double[Vi.getNumberOfElements()];
+			memcpy(verticesRaw,&*Vi.begin(),sizeof(float)*Vi.getNumberOfElements());
+
+			CellArray facets = inputs[i+1];
+
+			Polyhedron P = createPolyhedron(verticesRaw,numRows,facets);
+			Nef_polyhedron N(P);
+			if (i==0) {
+				Nf = Nf + N;
+			} else {
+				Nf = Nf - N;
+			}
 		}
 
-		verticesRaw = mxGetPr(prhs[i]);
+		// for (int i = 0; i < N; ++i) {
+		// 	Polygon_2 polyi = partition_polys[i];
+		// 	int M = polyi.size();
+		// 	TypedArray<double> outi =  f.createArray<double>({ (long unsigned int)M, 2});
+        //
+		// 	int j = 0;
+		// 	for (auto &elem : outi) {
+		// 		int k = j%M;
+		// 		Point_2 p = polyi.vertex(k);
+		// 		if (j < M)
+		// 			elem = (double) p.x();
+		// 		else
+		// 			elem = (double) p.y();
+		// 		j++;
+		// 	}
+		// 	out[i] = outi;
+		// }
 
-		Polyhedron P = createPolyhedron(verticesRaw,mrowsV,prhs[i+1]);
-		Nef_polyhedron N(P);
-		if (i==0) {
-			Nf = Nf + N;
-		} else {
-			Nf = Nf - N;
-		}
+		// std::cout << "num poly in: " << num_poly_in << std::endl;
 
-	}
-	std::list<Polyhedron> convex_parts = decompose(Nf);
-	mwSize num_polyhedrons = convex_parts.size();
+		std::list<Polyhedron> convex_parts = decompose(Nf);
+		size_t num_out = convex_parts.size();
+		CellArray out = f.createCellArray({1,num_out});
 
-	/* plhs[0] = mxCreateDoubleMatrix( 0, 0, mxREAL ); */
-	/* return; */
-
-	plhs[0] = mxCreateCellArray(1, &num_polyhedrons);
-	/* y = mxGetPr(plhs[0]); */
-	int cell_index = 0;
-	int i = 0;
-	// for (auto poly = convex_parts.begin(); poly != convex_parts.end(); ++poly) {
-	// 	MeshPolyhedron MP;
-	// 	poly2mesh(*poly,MP);
-	// 	std::cout << "Size of polyhedron " << i << ": " << MP.vertices.size() << std::endl;	
-	// 	i++;
-	// }
-	for (auto poly : convex_parts) {
-		MeshPolyhedron MP;
-		poly2mesh(poly,MP);
-		int num_vertices = poly.size_of_vertices();
-		mxArray *V = mxCreateDoubleMatrix( num_vertices, 3, mxREAL);
-		double *Vptr = mxGetPr(V);
-		/* std::cout << "size of vertices: " << num_vertices << std::endl; */
-		int k = 0;
-		for (auto v : MP.vertices) {
-			/* std::cout << v << " \n"; */
-			Vptr[k] = CGAL::to_double(v[0]);
-			Vptr[k + num_vertices] = CGAL::to_double(v[1]);
-			Vptr[k + num_vertices*2] = CGAL::to_double(v[2]);
-			k++;
-		}
-
-		mwSize num_facets = MP.facets.size();
-		/* std::cout << "size of vertices: " << num_vertices << std::endl; */
-		mxArray *facetsCell = mxCreateCellArray(1, &num_facets);
-		int facet_index = 0;
-		for (auto facet : MP.facets) {
-			int num = facet.size();
-			mxArray *F = mxCreateDoubleMatrix( num, 1, mxREAL);
-			double *Fptr = mxGetPr(F);
-			k=0;
-			for (auto i : facet) {
-				Fptr[k] = (double)i+1; //add 1 before returnin to matlab(indices start at 1)
+		int cell_index = 0;
+		int i = 0;
+		for (auto poly : convex_parts) {
+			MeshPolyhedron MP;
+			poly2mesh(poly,MP);
+			size_t num_vertices = poly.size_of_vertices();
+			int k = 0;
+			TypedArray<double> Vi = f.createArray<double>({num_vertices,3});
+			for (auto v : MP.vertices) {
+				Vi[k] = CGAL::to_double(v[0]);
+				Vi[k + num_vertices] = CGAL::to_double(v[1]);
+				Vi[k + num_vertices*2] = CGAL::to_double(v[2]);
 				k++;
 			}
-			mxSetCell(facetsCell,facet_index,F);
-			facet_index++;
+
+			size_t num_facets = MP.facets.size();
+			CellArray facetsCell = f.createCellArray({1,num_facets});
+			size_t num;
+			int facet_index = 0;
+			for (auto facet : MP.facets) {
+				num = facet.size();
+				TypedArray<double> F = f.createArray<double>({num,1});
+				k=0;
+				for (auto i : facet) {
+					F[k] = (double)i+1; //add 1 before returnin to matlab(indices start at 1)
+					k++;
+				}
+				facetsCell[facet_index] = F;
+				facet_index++;
+			}
+
+			num = 2;
+			CellArray polyCell = f.createCellArray({1,num});
+			polyCell[0] = Vi;
+			polyCell[1] = facetsCell;
+			out[cell_index++] = polyCell;
+		}
+		outputs[0] = out;
+	}
+
+	void checkArguments(ArgumentList outputs, ArgumentList inputs) {
+		// Get pointer to engine
+		std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
+
+		// Get array factory
+		ArrayFactory factory;
+
+		if (inputs.size() < 2)
+		{
+			matlabPtr->feval(u"error",
+					0,
+					std::vector<Array>({ factory.createScalar("Error: it must have 1 one input") }));
 		}
 
-		mwSize num = 2;
-		mxArray *polyCell = mxCreateCellArray(1, &num);
-		mxSetCell(polyCell,0,V);
-		mxSetCell(polyCell,1,facetsCell);
-		mxSetCell(plhs[0],cell_index,polyCell);
-		cell_index++;
-	}
-
-	return;
-}
-
-void poly2mesh(Polyhedron P, MeshPolyhedron &MP)
-{
-	std::size_t i = 0; 
-	for(Vertex_iterator v = P.vertices_begin(); v != P.vertices_end(); ++v)
-		  MP.vertices.push_back(v->point());
-
-	for ( Facet_iterator f = P.facets_begin(); f != P.facets_end(); ++f) {
-		i++;
-		HF_circulator hinit = f->facet_begin();
-		HF_circulator h = hinit;
-		Facet facet;
-		do {
-			Point p = h->vertex()->point();
-			int index = find(MP.vertices,p);
-			facet.push_back(index);
-			h++;
-		} while(h!=hinit);
-		MP.facets.push_back(facet);
-	}
-}
-
-int find(VertexList vs, Point p) 
-{
-	int index = 0;
-	for(auto v : vs) {
-		if (p[0]==v[0] && p[1]==v[1] && p[2]==v[2])
-			return index;
-		index++;
-	}
-	return -1;
-}
-
-/**
- * verticesRaw:  
- *
- */
-Polyhedron createPolyhedron(double *verticesRaw, int num_vertices, const mxArray *mxFacetsRaw)
-{
-	int num_facets = mxGetNumberOfElements(mxFacetsRaw);
-	// Assign pointers to each input and output.
-	const mxArray *cell_element_ptr;
-	double *facetRaw;
-	
-	FacetList facets;
-	for (int i = 0; i < num_facets; ++i) {
-		std::vector<int> facet;
-		cell_element_ptr = mxGetCell(mxFacetsRaw,i);
-		mwSize mrowsi = mxGetM(cell_element_ptr);
-		mwSize ncolsi = mxGetN(cell_element_ptr);
-		double *facetRaw = mxGetPr(cell_element_ptr);
-		
-		for (int k = 0; k < mrowsi; ++k) {
-			int index = facetRaw[k];
-			facet.push_back(index-1); //compensate for matlab stating with 1 on its indexes
-		}
-		facets.push_back(facet);
-	}
-
-
-	VertexList vertices;
-	for (int i = 0; i < num_vertices; ++i)
-		vertices.push_back(Point(verticesRaw[i], verticesRaw[num_vertices+i], verticesRaw[2*num_vertices+i]));
-	
-	Polyhedron P;
-	BuildPolyhedron<HalfedgeDS> poly(vertices,facets);
-	P.delegate(poly);
-	return P;
-}
-
-std::list<Polyhedron> decompose(Polyhedron P)
-{
-	Nef_polyhedron N(P);
-	return decompose(N);
-}
-
-std::list<Polyhedron> decompose(Nef_polyhedron N)
-{
-	/* CGAL_assertion( P.is_triangle( P.halfedges_begin())); */
-
-	CGAL::convex_decomposition_3(N);
-	std::list<Polyhedron> convex_parts;
-
-	// the first volume is the outer volume, which is 
-	// ignored in the decomposition
-	Volume_const_iterator ci = ++N.volumes_begin();
-	for( ; ci != N.volumes_end(); ++ci) {
-		if(ci->mark()) {
-			Polyhedron P;
-			N.convert_inner_shell_to_polyhedron(ci->shells_begin(), P);
-			convex_parts.push_back(P);
+		// Check number of outputs
+		if (outputs.size() > 1) 
+		{
+			matlabPtr->feval(u"error",
+					0,
+					std::vector<Array>({ factory.createScalar("Too many output arguments") }));
 		}
 	}
-	// std::cout << "decomposition into " << convex_parts.size() << " convex parts " << std::endl;
 
-	return convex_parts;
-}
+	void poly2mesh(Polyhedron P, MeshPolyhedron &MP)
+	{
+		std::size_t i = 0; 
+		for(Vertex_iterator v = P.vertices_begin(); v != P.vertices_end(); ++v)
+			MP.vertices.push_back(v->point());
+
+		for ( Facet_iterator f = P.facets_begin(); f != P.facets_end(); ++f) {
+			i++;
+			HF_circulator hinit = f->facet_begin();
+			HF_circulator h = hinit;
+			Facet facet;
+			do {
+				Point p = h->vertex()->point();
+				int index = find(MP.vertices,p);
+				facet.push_back(index);
+				h++;
+			} while(h!=hinit);
+			MP.facets.push_back(facet);
+		}
+	}
+
+	int find(VertexList vs, Point p) 
+	{
+		int index = 0;
+		for(auto v : vs) {
+			if (p[0]==v[0] && p[1]==v[1] && p[2]==v[2])
+				return index;
+			index++;
+		}
+		return -1;
+	}
+
+	/**
+	 * verticesRaw:  
+	 *
+	 */
+	Polyhedron createPolyhedron(double *verticesRaw, int num_vertices, const CellArray facets)
+	{
+		size_t num_facets = facets.getNumberOfElements();
+		// Assign pointers to each input and output.
+		double *facetRaw;
+
+		FacetList facets_lst;
+		for (int i = 0; i < num_facets; ++i) {
+			std::vector<int> facet;
+			TypedArray<double> Fi = facets[i];
+
+			for (auto elem : Fi)
+				facet.push_back(elem-1); //compensate for matlab stating with 1 on its indexes
+			facets_lst.push_back(facet);
+		}
+
+
+		VertexList vertices;
+		for (int i = 0; i < num_vertices; ++i)
+			vertices.push_back(Point(verticesRaw[i], verticesRaw[num_vertices+i], verticesRaw[2*num_vertices+i]));
+
+		Polyhedron P;
+		BuildPolyhedron<HalfedgeDS> poly(vertices,facets_lst);
+		P.delegate(poly);
+		return P;
+	}
+
+	std::list<Polyhedron> decompose(Polyhedron P)
+	{
+		Nef_polyhedron N(P);
+		return decompose(N);
+	}
+
+	std::list<Polyhedron> decompose(Nef_polyhedron N)
+	{
+		/* CGAL_assertion( P.is_triangle( P.halfedges_begin())); */
+
+		CGAL::convex_decomposition_3(N);
+		std::list<Polyhedron> convex_parts;
+
+		// the first volume is the outer volume, which is 
+		// ignored in the decomposition
+		Volume_const_iterator ci = ++N.volumes_begin();
+		for( ; ci != N.volumes_end(); ++ci) {
+			if(ci->mark()) {
+				Polyhedron P;
+				N.convert_inner_shell_to_polyhedron(ci->shells_begin(), P);
+				convex_parts.push_back(P);
+			}
+		}
+		// std::cout << "decomposition into " << convex_parts.size() << " convex parts " << std::endl;
+
+		return convex_parts;
+	}
+};
 
